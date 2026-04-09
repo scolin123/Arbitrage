d8358354d19b826ab4bbe5a580916fcb2fc1b973
cshier <colin@shier.ca>
2026-04-09 00:26:06 -0400

    oddsline class

--- models/odds.py
+++ models/odds.py
@@ -14,3 +14,16 @@ class MarketType(Enum):
     TOTAL = "total"
     THREE_WAY = "threeway"
 
+@dataclass
+class Oddsline:
+    book: str #Draftkings,Fanduel,Betmgm, Caesars
+    event_id: str   
+    sport: str  #NBA, NFL, NBA, NHL
+    market_type: MarketType
+    outcome_label: str  #Home, Away, Over, Under, Draw
+    raw_odds: str   #Original string scraped as e.g. -110
+    odds_format: OddsFormat
+    decimal_oods: float
+    scraped_at: datetime = field(defulat_factory = datetime.utcnow)
+    bet_url = str = ""
+
