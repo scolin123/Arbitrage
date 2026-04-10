f148fb524d113df91d94ac0f8525a02740800143
cshier <colin@shier.ca>
2026-04-09 23:09:23 -0400

    Arb leg class

--- models/arb.py
+++ models/arb.py
@@ -1,3 +1,10 @@
 from dataclass import dataclass,field
 from datetime import datetime
 from models.odds import OddsLine
+
+@dataclass
+class ArbLeg:
+    odds_line: OddsLine
+    stake: float
+    profit_if_win: float
+
