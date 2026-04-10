3a4992c03ae83c0e2ae07cefbff2e90c90a2be56
cshier <colin@shier.ca>
2026-04-09 23:39:10 -0400

    Odds converter tester

--- normalizer/odds_converter.py
+++ normalizer/odds_converter.py
@@ -28,4 +28,17 @@ class OddsConverter:
             numerator, denominator = raw.split("/")
             return OddsConverter.fractional_to_decimal(int(numerator), int(denominator))
 
+if __name__ == "__main__":
+    # American
+    print(OddsConverter.american_to_decimal(150))    # expect 2.5
+    print(OddsConverter.american_to_decimal(-110))   # expect 1.909091
+    print(OddsConverter.american_to_decimal(-200))   # expect 1.5
 
+    # Fractional
+    print(OddsConverter.fractional_to_decimal(5, 2)) # expect 3.5
+
+    # parse_and_normalize
+    print(OddsConverter.parse_and_normalize("+150", OddsFormat.AMERICAN))  # 2.5
+    print(OddsConverter.parse_and_normalize("EVEN", OddsFormat.AMERICAN))  # 2.0
+    print(OddsConverter.parse_and_normalize("5/2", OddsFormat.FRACTIONAL)) # 3.5
+    print(OddsConverter.parse_and_normalize("2.50", OddsFormat.DECIMAL))   # 2.5
